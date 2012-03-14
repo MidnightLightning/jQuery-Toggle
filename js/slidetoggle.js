36e65dfcf2a19d7ff01c825f904ceb23f7cf052b
Brooks Boyd <boydb@midnightdesign.ws>
2012-03-14 13:36:46 -0500

    Use jQuery UI widget model, and adhere to UI CSS styles

--- js/slidetoggle.js
+++ js/slidetoggle.js
@@ -1,4 +1,5 @@
 // jQuery plugin patterns: http://coding.smashingmagazine.com/2011/10/11/essential-jquery-plugin-patterns/
+// http://bililite.nfshost.com/blog/understanding-jquery-ui-widgets-a-tutorial/
 // jQuery widgets: http://ajpiano.com/widgetfactory
 (function($){
 	$.widget('md.slideToggle', {
@@ -8,38 +9,64 @@
 			// this.options -- options hash
 			var $input = this.element; // The input checkbox
 			
-			var $sliderContainer = $input.wrap('<div class="sliderContainer" />').parent(); // Wrap the element in a container div, and return a reference to the container
-			$sliderContainer.css('width', this.options.width).append('<div class="sliderTrack"><div class="sliderOnText"></div><div class="sliderPin"></div><div class="sliderOffText"></div></div>');
+			var $sliderContainer = $input.wrap('<div class="sliderContainer ui-widget ui-helper-reset ui-corner-all" />').parent(); // Wrap the element in a container div, and return a reference to the container
+			$sliderContainer.css('width', this.options.width).append('<div class="sliderTrack"><div class="sliderOnText ui-state-active ui-state-highlight"></div><div class="sliderPin"></div><div class="sliderOffText ui-state-default"></div></div>');
 			$input.hide(); // Hide base checkbox
-			$sliderContainer.find('.sliderOnText')
-				.html(this.options.checkedText)
-				.on("mousedown.slidetoggle", { direction: 'turnOff'}, this.toggle);
-			$sliderContainer.find('.sliderOffText')
-				.html(this.options.uncheckedText)
-				.on("mousedown.slidetoggle", { direction: 'turnOn'}, this.toggle);
 			
 			// Save references for the future
 			this.sliderContainer = $sliderContainer;
 			this.sliderTrack = $sliderContainer.find('.sliderTrack');
 			this.sliderPin = $sliderContainer.find('.sliderPin');
+			this.sliderContainer.data('widget', this); // Give the container a reference to the widget; for mouse events
+			console.log(this.sliderContainer.data(), this.sliderContainer.data('widget'));
+
+			$sliderContainer.find('.sliderOnText')
+				.html(this.options.checkedText)
+				.on("mousedown.slidetoggle", {w: this}, this._clickToggle);
+			$sliderContainer.find('.sliderOffText')
+				.html(this.options.uncheckedText)
+				.on("mousedown.slidetoggle", {w: this}, this._clickToggle);
+		},
+		_init: function() {
+			this.sliderPin.css('left', Math.floor(this.sliderTrack.width()/2-this.sliderPin.width()/2) + 'px'); // Move pin to center of track
+			if (this.element.prop('checked')) {
+				this.turnOn(false); // Set to "on" position
+			} else {
+				this.turnOff(false); // Set to "off" position
+			}
 		},
 		options: { // Special variable; treated by jQuery as individual to each instance of the widget, and override-able on initial call
 			checkedText: 'On',
 			uncheckedText: 'Off',
 			size: '50px',
 			isChecked: false,
-			pinPadding: 3
+			pinPadding: 2
 		},
 		_offPos: function() {
 			return Math.floor(this.sliderTrack.width()/2 * -1 + this.sliderPin.width()/2 + this.options.pinPadding) + 'px';
 		},
 		_onPos: function() {
-			Math.floor(this.sliderTrack.width()/2 * -1 + this.sliderContainer.width() - this.sliderPin.width()/2 - this.options.pinPadding) + 'px'
+			return Math.floor(this.sliderTrack.width()/2 * -1 + this.sliderContainer.width() - this.sliderPin.width()/2 - this.options.pinPadding) + 'px';
+		},
+		_clickToggle: function(e) {
+			// This is a mouse event response; find the actual widget code
+			var $container = $(this).parentsUntil('.sliderContainer').parent();
+			var w = $container.data('widget');
+			w.toggle();
+		},
+		toggle: function(animate) {
+			if (this.options.isChecked) {
+				this.turnOff(animate); // Currently on; turn off
+			} else {
+				this.turnOn(animate); // Currently off; turn on
+			}
 		},
 		turnOff: function(animate) {
+			var animate = animate || true; // If undefined, default to true
+			console.log("turning off", animate);
 			if (animate) {
 				this.sliderTrack
-					.css('left', this._onPos()) // Reset to "on" position
+					.stop().css('left', this._onPos()) // Reset to "on" position
 					.animate({left: this._offPos()}, 'fast'); // Slide to "off" position
 			} else {
 				this.sliderTrack.css('left', this._offPos()); // Move to "off" position
@@ -48,9 +75,11 @@
 			this.options.isChecked = false;
 		},
 		turnOn: function(animate) {
+			var animate = animate || true; // If undefined, default to true
+			console.log("turning on", animate);
 			if (animate) {
 				this.sliderTrack
-					.css('left', this._offPos()) // Reset to "off" position
+					.stop().css('left', this._offPos()) // Reset to "off" position
 					.animate({left: this._onPos()}, 'fast'); // Slide to "on" position
 			} else {
 				this.sliderTrack.css('left', this._onPos()); // Move to "on" position
